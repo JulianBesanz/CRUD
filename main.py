8bd14afcd5d032fddc158e211bc2400696e43cbd
JulianBesanz <darkvaan97@gmail.com>
2025-05-15 20:08:20 -0500

    15_mayo_clase

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for,redirect
 
 app = Flask(__name__)
 
 usuarios = []
+id_contador =1
 
 
 
@@ -10,11 +11,25 @@ usuarios = []
 
 @app.route("/", methods=["GET", "POST"])
 def crud():
-    nombre = request.form.get("nombre")
-    email = request.form.get("email")
-    usuarios.append({"username": nombre, "email": email })
+    global id_contador
 
-    return render_template("registro.html", usuarios=usuarios)
+    if request.method=='POST':
+        nombre = request.form.get("nombre")
+        email = request.form.get("email")
+        usuarios.append({"id":id_contador, "username": nombre, "email": email }) #insertando un usuario
+        id_contador +=1
+        return redirect(url_for("crud"))#llamar nombre de la funcion
+
+    id_eliminar=request.args.get("borrar")
+    if id_eliminar:
+            #TODO: eliminar el usuario con el id del parametro de la lista
+            for item in usuarios:
+                if str(item['id'])==id_eliminar:
+                    usuarios.remove(item)
+                    break
+            return redirect(url_for("crud"))# LLLAMAR NOMBRE DE LA FUNCION
+
+    return render_template("registro.html", usuarios=usuarios) # lista que entregamos al html
 
 
 
